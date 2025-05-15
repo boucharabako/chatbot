23a09a0d274ca30da166077f9900bc9bc5758352
BBUSHRA <boucharabako.bb@gmail.com>
2025-05-15 20:16:29 +0000

    file jenkins supprimé

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from mangum import Mangum
+
+from .utils import Utils
+
+
+@asynccontextmanager
+async def app_lifespan(application: FastAPI):
+    Utils.log_info("Starting the application")
+    yield
+
+
+app = FastAPI(
+    title="Chatbot API",
+    description="Chatbot API description",
+    version="1.0.0",
+    lifespan=app_lifespan,
+)
+
+
+@app.get("/")
+async def root():
+    return {"msg": "Hello World"}
+
+
+handler = Mangum(app)
